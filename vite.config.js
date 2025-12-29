4201c8b3ac346656829095322bbc140fe8c18a7c
heese <heese@heeseui-MacBookAir.local>
2025-12-29 11:50:21 +0900

    Fix build: Disable minification and target esnext

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     build: {
-        target: 'es2020'
+        target: 'esnext',
+        minify: false
     },
     optimizeDeps: {
         esbuildOptions: {
-            target: 'es2020'
+            target: 'esnext' // Ensure dev mode also uses latest target
         }
     }
 })
