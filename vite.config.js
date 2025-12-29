39d2edb7bbe2277ee608fa996996157fdefc827e
heese <heese@heeseui-MacBookAir.local>
2025-12-29 11:48:02 +0900

    Fix Firestore assertion error: Update build target to es2020

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        target: 'es2020'
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            target: 'es2020'
+        }
+    }
 })
